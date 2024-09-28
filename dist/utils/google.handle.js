e56f6f54366062e524f17f23d5ff29aafa826a87
Sergio Maté Suárez <sergi.matsua@gmail.com>
2024-09-28 17:20:19 +0200

    Selectores arreglados. informacion completa

--- dist/utils/google.handle.js
+++ dist/utils/google.handle.js
@@ -16,8 +16,10 @@ exports.handleScrapGoogleMaps = exports.handleGetLocationByLink = exports.handle
 require("dotenv/config");
 const Site_1 = require("../interfaces/Site");
 const puppeteer_1 = __importDefault(require("puppeteer"));
+const fs_1 = __importDefault(require("fs"));
 const chrome_aws_lambda_1 = __importDefault(require("chrome-aws-lambda"));
 const ScrappedSite_1 = require("../interfaces/ScrappedSite");
+const COOKIES_FILE_PATH = './cookies.json'; // Archivo donde se guarda la sesión (cookies y localStorage)
 const handleFindSitesByTextGoogle = (text) => __awaiter(void 0, void 0, void 0, function* () {
     const centro = {
         latitude: 43.34918,
@@ -79,49 +81,66 @@ const handleScrapGoogleMaps = (query) => __awaiter(void 0, void 0, void 0, funct
     const browser = yield puppeteer_1.default.launch({
         args: [
             ...chrome_aws_lambda_1.default.args,
-            '--lang=es-ES'
         ],
         executablePath: yield chrome_aws_lambda_1.default.executablePath, // Ruta al binario de Chromium
         headless: true, // Asegura que se ejecute en modo sin interfaz gráfica
     });
-    const page = yield browser.newPage();
     //sustituir espacios por +
     query = query.replace(' ', '+');
-    const url = 'https://www.google.com/maps/search/'.concat(query) + '?hl=en';
-    // Evita que se carguen las hojas de estilo e imagenes innecesarias
-    // await page.setRequestInterception(true);
-    // page.on('request', req => {
-    //     if (req.resourceType() === 'image' || req.resourceType() === 'stylesheet') {
-    //         req.abort();
-    //     } else {
-    //         req.continue();
-    //     }
-    // });
+    const url = 'https://www.google.com/maps/search/'.concat(query) + '?hl=es';
+    const page = yield browser.newPage();
     yield page.goto(url);
+    // const cookiesLoaded = await loadCookiesFromFile(page, COOKIES_FILE_PATH);
     // Selector que encuentra un div con un aria-label que contiene la palabra clave
     const selectorResultList = '.Nv2PK';
-    const selectorRejectCookies = '[aria-label*="Reject all"]';
+    const selectorAcceptCookies = '[aria-label*="Aceptar todo"]';
+    // if (!cookiesLoaded || cookiesLoaded.length === 0 || areCookiesExpired(cookiesLoaded).hasExpired) {
+    // console.log("first time");
     // Espera a que el botón de rechazo de cookies sea detectable
-    yield page.waitForSelector(selectorRejectCookies, { timeout: 10000 });
+    yield page.waitForSelector(selectorAcceptCookies, { timeout: 5000 });
     // Hace clic en el botón de rechazo de cookies
-    yield page.click(selectorRejectCookies);
+    yield page.click(selectorAcceptCookies);
+    // Guardar cookies en un archivo
+    // await saveCookiesToFile(page, COOKIES_FILE_PATH);
     // Espera a que cualquier redirección potencial o recarga de la página termine
     yield page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+    // }
+    // console.log("cookies loaded");
     const sitesData = yield page.evaluate((selector) => {
         const elements = Array.from(document.querySelectorAll(selector));
         return elements.map(el => {
             var _a, _b, _c, _d, _e;
             const nombre = ((_a = el.querySelector('.qBF1Pd')) === null || _a === void 0 ? void 0 : _a.textContent) || '';
-            const direccion = ((_b = el.querySelector('.W4Efsd .W4Efsd span:nth-of-type(2) span:nth-of-type(2)')) === null || _b === void 0 ? void 0 : _b.textContent) || '';
+            let direccion = ((_b = el.querySelector('.W4Efsd .W4Efsd > span:nth-child(3)')) === null || _b === void 0 ? void 0 : _b.textContent) || '';
             const tipo = ((_c = el.querySelector('.W4Efsd .W4Efsd > span > span')) === null || _c === void 0 ? void 0 : _c.textContent) || '';
-            const rating = ((_d = el.querySelector('.MW4etd')) === null || _d === void 0 ? void 0 : _d.textContent) || '0';
+            const calificacionGoogle = ((_d = el.querySelector('.MW4etd')) === null || _d === void 0 ? void 0 : _d.textContent) || '0';
             const link = ((_e = el.querySelector('.hfpxzc')) === null || _e === void 0 ? void 0 : _e.getAttribute('href')) || '';
-            return { nombre, direccion, rating: parseFloat(rating), tipos: [tipo], link };
+            // Eliminar todos los " . " de la dirección
+            direccion = direccion.replace(' · ', '').trim();
+            // Extraer latitud y longitud del enlace usando !3d para latitud y !4d para longitud
+            const latitudRegex = /!3d(-?\d+(\.\d+)?)/;
+            const longitudRegex = /!4d(-?\d+(\.\d+)?)/;
+            const latitudMatch = link.match(latitudRegex);
+            const longitudMatch = link.match(longitudRegex);
+            const latitud = latitudMatch ? parseFloat(latitudMatch[1]) : null;
+            const longitud = longitudMatch ? parseFloat(longitudMatch[1]) : null;
+            // Crear el objeto location si se encuentran coordenadas
+            const location = (latitud !== null && longitud !== null) ? { latitud, longitud } : undefined;
+            return {
+                nombre,
+                direccion,
+                calificacionGoogle: parseFloat(calificacionGoogle),
+                tipos: [tipo],
+                link,
+                location
+            };
         });
     }, selectorResultList);
-    // Ahora, fuera de page.evaluate, crea instancias de ScrappedSite con los datos recolectados.
-    const sites = sitesData.map((siteData) => new ScrappedSite_1.ScrappedSite(siteData.nombre, siteData.direccion, siteData.rating, siteData.tipos, siteData.link));
-    console.log(sites);
+    // Crear instancias de ScrappedSite con los datos recolectados.
+    const sites = sitesData.map((siteData) => new ScrappedSite_1.ScrappedSite(siteData.nombre, siteData.direccion, siteData.calificacionGoogle, siteData.tipos, siteData.link, siteData.location, // Se pasa el objeto de ubicación (latitud, longitud)
+    siteData.calificacionGoogle, // Se usa calificacionGoogle también para rating
+    siteData.tipos // Se pasa la lista de tipos dos veces (types y tipos)
+    ));
     yield browser.close();
     return sites;
 });
@@ -135,5 +154,59 @@ function convertToSite(placeResponse) {
             longitude: lng
         };
         return new Site_1.Site(place_id, name, formatted_address, rating, location, types);
+    });
+}
+const areCookiesExpired = (cookies) => {
+    const now = Date.now(); // Obtiene la hora actual en milisegundos
+    const expiredCookies = cookies.filter(cookie => {
+        // Verifica si la cookie tiene un campo de expiración y si ha expirado
+        return cookie.expires && cookie.expires < now;
+    });
+    // Devuelve un objeto que contiene el estado y las cookies expiradas
+    return {
+        hasExpired: expiredCookies.length > 0,
+        expiredCookies: expiredCookies,
+    };
+};
+function saveCookiesToFile(page, filePath) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            // Getting the cookies from the current page
+            const cookies = yield page.cookies();
+            // Writing the cookies to a file as JSON
+            const fs = require('fs');
+            fs.writeFileSync(filePath, JSON.stringify(cookies, null, 2));
+            // Cookies have been saved successfully
+            return true;
+        }
+        catch (error) {
+            // An error occurred while saving cookies
+            console.error('Error saving cookies:', error);
+            return false;
+        }
+    });
+}
+function loadCookiesFromFile(page, filePath) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            // Check if the cookies file exists
+            if (!fs_1.default.existsSync(filePath)) {
+                // If the file does not exist, create an empty cookies array and write it to the file
+                fs_1.default.writeFileSync(filePath, JSON.stringify([], null, 2));
+                console.log('Archivo de cookies creado:', filePath);
+            }
+            // Reading cookies from the specified file
+            const cookiesJson = fs_1.default.readFileSync(filePath, 'utf-8');
+            const cookies = JSON.parse(cookiesJson);
+            // Setting the cookies in the current page
+            yield page.setCookie(...cookies);
+            console.log('Cookies cargadas correctamente:', cookies);
+            return cookies;
+        }
+        catch (error) {
+            // An error occurred while loading cookies
+            console.error('Error al cargar las cookies:', error);
+            return null;
+        }
     });
 }
--- src/utils/google.handle.ts
+++ src/utils/google.handle.ts
@@ -1,10 +1,12 @@
 import "dotenv/config";
 import { Site, SiteLocation } from "../interfaces/Site";
-import puppeteer from "puppeteer";
+import puppeteer, { Page } from "puppeteer";
+import fs from 'fs';
 import chromium from "chrome-aws-lambda";
 import * as pluscodes from "pluscodes";
 import { ScrappedSite } from "../interfaces/ScrappedSite";
-import e from "express";
+
+const COOKIES_FILE_PATH = './cookies.json';  // Archivo donde se guarda la sesión (cookies y localStorage)
 
 export const handleFindSitesByTextGoogle = async (text: string) => {
     const centro: SiteLocation = {
@@ -77,58 +79,88 @@ export const handleScrapGoogleMaps = async (query: string) => {
     const browser = await puppeteer.launch({
         args: [
             ...chromium.args,
-            '--lang=es-ES'
         ],
         executablePath: await chromium.executablePath,  // Ruta al binario de Chromium
         headless: true,  // Asegura que se ejecute en modo sin interfaz gráfica
     });
-    const page = await browser.newPage();
 
     //sustituir espacios por +
     query = query.replace(' ', '+');
-    const url = 'https://www.google.com/maps/search/'.concat(query) + '?hl=en';
+    const url = 'https://www.google.com/maps/search/'.concat(query) + '?hl=es';
 
-    // Evita que se carguen las hojas de estilo e imagenes innecesarias
-    // await page.setRequestInterception(true);
-    // page.on('request', req => {
-    //     if (req.resourceType() === 'image' || req.resourceType() === 'stylesheet') {
-    //         req.abort();
-    //     } else {
-    //         req.continue();
-    //     }
-    // });
+    const page = await browser.newPage();
 
     await page.goto(url);
 
+    // const cookiesLoaded = await loadCookiesFromFile(page, COOKIES_FILE_PATH);
+
     // Selector que encuentra un div con un aria-label que contiene la palabra clave
     const selectorResultList = '.Nv2PK';
-    const selectorRejectCookies = '[aria-label*="Reject all"]';
+    const selectorAcceptCookies = '[aria-label*="Aceptar todo"]';
 
+    // if (!cookiesLoaded || cookiesLoaded.length === 0 || areCookiesExpired(cookiesLoaded).hasExpired) {
+    // console.log("first time");
     // Espera a que el botón de rechazo de cookies sea detectable
-    await page.waitForSelector(selectorRejectCookies, { timeout: 10000 });
+    await page.waitForSelector(selectorAcceptCookies, { timeout: 5000 });
 
     // Hace clic en el botón de rechazo de cookies
-    await page.click(selectorRejectCookies);
+    await page.click(selectorAcceptCookies);
+
+    // Guardar cookies en un archivo
+    // await saveCookiesToFile(page, COOKIES_FILE_PATH);
 
     // Espera a que cualquier redirección potencial o recarga de la página termine
     await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
-
+    // }
+    // console.log("cookies loaded");
     const sitesData = await page.evaluate((selector: any) => {
         const elements = Array.from(document.querySelectorAll(selector));
+
         return elements.map(el => {
             const nombre = el.querySelector('.qBF1Pd')?.textContent || '';
-            const direccion = el.querySelector('.W4Efsd .W4Efsd span:nth-of-type(2) span:nth-of-type(2)')?.textContent || '';
+            let direccion = el.querySelector('.W4Efsd .W4Efsd > span:nth-child(3)')?.textContent || '';
             const tipo = el.querySelector('.W4Efsd .W4Efsd > span > span')?.textContent || '';
-            const rating = el.querySelector('.MW4etd')?.textContent || '0';
+            const calificacionGoogle = el.querySelector('.MW4etd')?.textContent || '0';
             const link = el.querySelector('.hfpxzc')?.getAttribute('href') || '';
-            return { nombre, direccion, rating: parseFloat(rating), tipos: [tipo], link };
+
+            // Eliminar todos los " . " de la dirección
+            direccion = direccion.replace(' · ', '').trim();
+
+            // Extraer latitud y longitud del enlace usando !3d para latitud y !4d para longitud
+            const latitudRegex = /!3d(-?\d+(\.\d+)?)/;
+            const longitudRegex = /!4d(-?\d+(\.\d+)?)/;
+
+            const latitudMatch = link.match(latitudRegex);
+            const longitudMatch = link.match(longitudRegex);
+
+            const latitud = latitudMatch ? parseFloat(latitudMatch[1]) : null;
+            const longitud = longitudMatch ? parseFloat(longitudMatch[1]) : null;
+
+            // Crear el objeto location si se encuentran coordenadas
+            const location = (latitud !== null && longitud !== null) ? { latitud, longitud } : undefined;
+
+            return {
+                nombre,
+                direccion,
+                calificacionGoogle: parseFloat(calificacionGoogle),
+                tipos: [tipo],
+                link,
+                location
+            };
         });
     }, selectorResultList);
 
-    // Ahora, fuera de page.evaluate, crea instancias de ScrappedSite con los datos recolectados.
-    const sites = sitesData.map((siteData: any) => new ScrappedSite(siteData.nombre, siteData.direccion, siteData.rating, siteData.tipos, siteData.link));
-
-    console.log(sites);
+    // Crear instancias de ScrappedSite con los datos recolectados.
+    const sites = sitesData.map((siteData: any) => new ScrappedSite(
+        siteData.nombre,
+        siteData.direccion,
+        siteData.calificacionGoogle,
+        siteData.tipos,
+        siteData.link,
+        siteData.location,    // Se pasa el objeto de ubicación (latitud, longitud)
+        siteData.calificacionGoogle, // Se usa calificacionGoogle también para rating
+        siteData.tipos        // Se pasa la lista de tipos dos veces (types y tipos)
+    ));
 
     await browser.close();
 
@@ -165,3 +197,58 @@ interface PlaceResponse {
     }>;
 }
 
+
+const areCookiesExpired = (cookies: Array<any>) => {
+    const now = Date.now(); // Obtiene la hora actual en milisegundos
+    const expiredCookies = cookies.filter(cookie => {
+        // Verifica si la cookie tiene un campo de expiración y si ha expirado
+        return cookie.expires && cookie.expires < now;
+    });
+
+    // Devuelve un objeto que contiene el estado y las cookies expiradas
+    return {
+        hasExpired: expiredCookies.length > 0,
+        expiredCookies: expiredCookies,
+    };
+};
+
+async function saveCookiesToFile(page: Page, filePath: string) {
+    try {
+        // Getting the cookies from the current page
+        const cookies = await page.cookies();
+
+        // Writing the cookies to a file as JSON
+        const fs = require('fs');
+        fs.writeFileSync(filePath, JSON.stringify(cookies, null, 2));
+
+        // Cookies have been saved successfully
+        return true;
+    } catch (error) {
+        // An error occurred while saving cookies
+        console.error('Error saving cookies:', error);
+        return false;
+    }
+}
+async function loadCookiesFromFile(page: Page, filePath: string) {
+    try {
+        // Check if the cookies file exists
+        if (!fs.existsSync(filePath)) {
+            // If the file does not exist, create an empty cookies array and write it to the file
+            fs.writeFileSync(filePath, JSON.stringify([], null, 2));
+            console.log('Archivo de cookies creado:', filePath);
+        }
+
+        // Reading cookies from the specified file
+        const cookiesJson = fs.readFileSync(filePath, 'utf-8');
+        const cookies = JSON.parse(cookiesJson);
+
+        // Setting the cookies in the current page
+        await page.setCookie(...cookies);
+        console.log('Cookies cargadas correctamente:', cookies);
+        return cookies;
+    } catch (error) {
+        // An error occurred while loading cookies
+        console.error('Error al cargar las cookies:', error);
+        return null;
+    }
+}
